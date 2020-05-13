ab336cc4e9db419ca634755464d9994b6d67cf98
Piggey <49120531+Piggey@users.noreply.github.com>
2020-05-13 21:37:55 +0200

    added pp value from a score

--- otg.py
+++ otg.py
@@ -19,9 +19,9 @@ def downloadImgFromLink(URL, new_filepath):
             handle.write(block)
     return new_filepath
 
-def fetchBeatmapCover(_artist, _title, _mapper):
+def checkBeatmap(artist_, title_, mapper_):
     checked = []
-    response = api.get_beatmaps(_mapper)
+    response = api.get_beatmaps(mapper_)
     for beatmap in response:
         artist = beatmap['artist']
         title = beatmap['title']
@@ -30,16 +30,20 @@ def fetchBeatmapCover(_artist, _title, _mapper):
             pass
         checked.append(artist+title)
 
-        if(_artist.lower() == artist.lower() and _title.lower() == title.lower()):
-            bg_id = beatmap['beatmapset_id']
-            print(f'[*] cover id: {bg_id}')
-            bg_url = f'https://assets.ppy.sh/beatmaps/{bg_id}/covers/cover.jpg'
-            return downloadImgFromLink(bg_url, 'temp/cover.jpg')
-    return '[*] BACKGROUND NOT FOUND'
+        if(artist_.lower() == artist.lower() and title_.lower() == title.lower()):
+            return beatmap
+    return -1
+
+def fetchBeatmapCover(beatmap):
+    if(beatmap != -1):
+        bgset_id = beatmap['beatmapset_id']
+        print(f'[*] cover id: {bgset_id}')
+        bg_url = f'https://assets.ppy.sh/beatmaps/{bgset_id}/covers/cover.jpg'
+        return downloadImgFromLink(bg_url, 'temp/cover.jpg')
 
 def checkAPI():
     if(not exists('API_KEY')):
-        API = input('Please paste your osu! api key (link here lololol): ')
+        API = input('Please paste your osu! api key (link to API site here): ')
         f = open('API_KEY', 'w')
         f.write(API)
         f.close()
@@ -100,9 +104,14 @@ print('[*] downloading player avatar')
 player_id = api.get_user(data[4])[0]['user_id']
 player_avatar = downloadImgFromLink(f'http://s.ppy.sh/a/{player_id}', 'temp/player_avatar.jpg')
 
-print('[*] downloading the beatmap cover')
-map_cover = fetchBeatmapCover(data[0], data[1], data[3])
-editThumbnail(map_cover, data[0], data[1], data[4], data[2], player_avatar)
+print('[*] downloading beatmap cover')
+beatmap = checkBeatmap(data[0], data[1], data[3])
+map_cover = fetchBeatmapCover(beatmap)
+bg_id = beatmap['beatmap_id']
+
+pp = str(round(float(api.get_scores(bg_id, data[4])[0]['pp']))) + ' pp'
+print('[*] pp value: ' + pp)
+editThumbnail(map_cover, data[0], data[1], data[4], data[2], player_avatar, pp)
 
 # remove unnecessary files
 print('[*] removing temporary files')
